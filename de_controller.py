ef840aecc946487cdfbe02f8c48fe0fd765b8610
Rafael Cavalcanti <rafael.c@fineandcountry.com>
2025-10-22 11:31:57 +0100

    refactor: adjusted load process

--- de_controller.py
+++ de_controller.py
@@ -1,5 +1,5 @@
 from de_extract import stage_inputs, get_base_path
-from de_load import save_selected_to_parquet, upload_selected_to_postgresql, upload_selected_to_bigquery
+from de_load import save_selected_to_parquet, upload_selected_to_postgresql, upload_selected_to_bigquery, clean_tmp_files
 
 def db_extract():
     base_path = get_base_path()
@@ -7,7 +7,7 @@ def db_extract():
     return "extracted"
 
 def db_transform():
-    save_selected_to_parquet({})
+    save_selected_to_parquet()
     return "transformed"
 
 def db_load():
@@ -19,3 +19,4 @@ if __name__ == "__main__":
     print(db_extract())
     print(db_transform())
     print(db_load())
+    clean_tmp_files()

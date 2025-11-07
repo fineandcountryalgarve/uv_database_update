2d884f6328c6628c72f504c7390b22701de3f44c
Rafael Cavalcanti <rafael.c@fineandcountry.com>
2025-11-07 17:20:27 +0000

    added de load

--- de_load.py
+++ de_load.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from app.utils.db_engine import get_engine
+from app.utils.db_engine import get_engine, kill_stale_sessions
 from app.utils.get_base_path import get_base_path
-from de_extract import stage_inputs
+from pathlib import Path
 
 base_path = get_base_path()
 
 engine = get_engine ()
 
-def save_selected_to_sql(selection=None):
-    all_files = stage_inputs()  # 👈 Reuse extract output
+def save_selected_to_sql(all_files, selection=None):
     selection = selection or list(all_files.keys())
 
     for name in selection:
@@ -17,15 +16,27 @@ def save_selected_to_sql(selection=None):
             print(f"⚠️ Skipping {name}: file not found at {xlsx_path}")
             continue
 
-        df = pd.read_excel(xlsx_path)
+        df = pd.read_excel ( 
+        xlsx_path,
+        engine="openpyxl",
+    )
         
-        df.to_sql(
-            name,
-            con=engine,
-            schema="bronze",
-            if_exists="replace",
-            index=False,
-            method="multi",
-            chunksize=5000
-        )
-        print(f"✅ {name}: {len(df):,} rows loaded into bronze.{name}")
+        with engine.begin() as conn:
+            kill_stale_sessions(engine)
+            df.to_sql(
+                name,
+                con=conn,
+                schema="bronze",
+                if_exists="replace",
+                index=False
+            )
+            print(f"✅ {name}: {len(df):,} rows loaded into bronze.{name}")
+
+def cleanup_tmp_folder():
+    base = Path(base_path)
+    for f in base.glob("*.xlsx"):
+        try:
+            f.unlink()
+            print(f"🧹 Deleted {f}")
+        except Exception as e:
+            print(f"⚠️ Could not delete {f}: {e}")

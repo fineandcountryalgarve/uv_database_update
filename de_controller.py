edf67e2ee08477a1fc65be74b84a9b9140fc55e9
Rafael Cavalcanti <rafael.c@fineandcountry.com>
2025-11-17 13:29:17 +0000

    updated controller

--- de_controller.py
+++ de_controller.py
@@ -1,6 +1,6 @@
 from os import path
 from de_extract import stage_inputs
-from de_load import save_selected_to_sql, cleanup_tmp_folder
+from de_load import save_selected_to_sql, upload_selected_to_bigquery, cleanup_tmp_folder
 from app.utils.get_base_path import get_base_path
 
 base_path = get_base_path
@@ -9,6 +9,7 @@ def upload_bronze():
     cleanup_tmp_folder()
     all_files = stage_inputs()
     save_selected_to_sql(all_files)
+    upload_selected_to_bigquery (all_files)
     print("✅ Bronze layer successfully updated.")
     cleanup_tmp_folder()
     return all_files
